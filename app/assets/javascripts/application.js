fcb2cb5f2b502fa0d0415c701c852173792fa69f
Eric Biel <eric.j.biel@gmail.com>
2017-10-11 11:24:40 -0500

    Add remaining e&f- items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,64 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+
+  // a-series hover effect
+  $('#a').hover(function() {
+    $('#a-drop').slideToggle(800);
+   });
+
+  $('#avf').hover(function() {
+    $('#avf-drop').slideToggle(800);
+  });
+
+  // b-series hover effect
+  $('#b').hover(function() {
+    $('#b-drop').slideToggle(800);
+  });
+
+  $('#c').hover(function() {
+    $('#c-drop').slideToggle(800);
+  });
+
+  $('#d').hover(function() {
+    $('#d-drop').slideToggle(800);
+  });
+
+  $('#e').hover(function() {
+    $('#e-drop').slideToggle(800);
+  });
+
+  $('#g').hover(function() {;
+    $('#g-drop').slideToggle(800);
+  });
+
+  $('#h').hover(function() {
+    $('#h-drop').slideToggle(800);
+  });
+
+  $('#s').hover(function() {
+    $('#s-drop').slideToggle(800);
+  });
+
+  $('#pre').hover(function() {
+    $('#pre-drop').slideToggle(800);
+  });
+
+  $('#fip').hover(function() {
+    $('#fip-drop').slideToggle(800);
+  });
+
+  $('#former').hover(function() {
+    $('#former-drop').slideToggle(800);
+  });
+
+  $('#unid').hover(function() {
+    $('#unid-drop').slideToggle(800);
+  });
+
+  $('.fa-bars').click(function() {
+    var btns = $('.btn-default').show();
+    $('.fa-bars::after').append(btns);
+  });
+});
