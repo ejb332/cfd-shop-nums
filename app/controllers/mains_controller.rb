b7b19da116136e71f8f10a397f0f6694b7c3b599
Eric Biel <eric.j.biel@gmail.com>
2018-03-29 22:39:35 -0500

    Update search

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -9,37 +9,47 @@ class MainsController < ApplicationController
     if search_term[1] != '-'
       search_term.insert(1, '-')
     end
-    # mostly works - refactor to case statement
+    # why won't this refactor to case statement?
+    # not DRY! should have 1 render statement
     if search_term.start_with?('AVF')
       @airports = Airport.where('shop_num = ?', "#{search_term}")
       @search_tables = @airports
+      render 'search.html.erb'
     elsif search_term.start_with?('A')
       @cars = Car.where('shop_num = ?', "#{search_term}")
       @search_tables = @cars
+      render 'search.html.erb'
     elsif search_term.start_with?('B')
       @suvs = Suv.where('shop_num = ?', "#{search_term}")
       @search_tables = @suvs
+      render 'search.html.erb'
     elsif search_term.start_with?('C')
       @ambulances = Ambulance.where('shop_num = ?', "#{search_term}")
       @search_tables = @ambulances
+      render 'search.html.erb'
     elsif search_term.start_with?('D')
       @engines = Engine.where('shop_num = ?', "#{search_term}")
       @search_tables = @engines
+      render 'search.html.erb'
     elsif search_term.start_with?('E', 'F')
       @trucks = Truck.where('shop_num = ?', "#{search_term}")
       @search_tables = @trucks
+      render 'search.html.erb'
     elsif search_term.start_with?('G')
       @specials = Special.where('shop_num = ?', "#{search_term}")
       @search_tables = @specials
+      render 'search.html.erb'
     elsif search_term.start_with?('H')
       @steamers = Steamer.where('shop_num = ?', "#{search_term}")
       @search_tables = @steamers
+      render 'search.html.erb'
     elsif search_term.start_with?('S')
       @others = Other.where('shop_num = ?', "#{search_term}")
       @search_tables = @others
+      render 'search.html.erb'
     else
-      flash[:notice] = "No results"
+      redirect_to root_url
+      flash[:notice] = 'No records found'
     end
-    render 'search.html.erb'
   end
 end
