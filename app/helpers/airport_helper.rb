e95456097873905f9c72bebcfe1d40f033643d2d
Eric Biel <eric.j.biel@gmail.com>
2018-10-16 20:55:55 -0500

    Fix image loading

--- app/helpers/airport_helper.rb
+++ app/helpers/airport_helper.rb
@@ -2,7 +2,7 @@ module AirportHelper
   def airport_picture
     if @airport.show_img == true
       image_tag(
-        "https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/cars/#{@car.id}.png"
+        "https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/airports/#{@airport.id}.png"
       )
     else
       image_tag('https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/No_image_available.svg')
--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -2,7 +2,7 @@ module CarsHelper
   def car_picture
     if @car.show_img == true
       image_tag(
-        "https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/airports/#{@airport.id}.png"
+        "https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/cars/#{@car.id}.png"
       )
     else
       image_tag('https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/No_image_available.svg')
