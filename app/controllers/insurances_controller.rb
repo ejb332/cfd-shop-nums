02804977c721435cad999811aa9c9afc132a4ab2
Eric Biel <eric.j.biel@gmail.com>
2019-08-25 17:50:33 -0500

    Remove unnecessary code from Insurances Controller

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -1,7 +1,7 @@
 class InsurancesController < ApplicationController
   def index
     @insurances = Insurance.all.order(id: :asc)
-    @photos = CfdShopNums::Application::S3_BUCKET.objects
+    # @photos = CfdShopNums::Application::S3_BUCKET.objects
     render 'index.html.erb'
   end
 end
