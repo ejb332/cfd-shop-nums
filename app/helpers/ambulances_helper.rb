ff665101d9c1bfd81c97d1a65751d82c371dc87d
Eric Biel <eric.j.biel@gmail.com>
2020-02-01 17:51:08 -0600

    Retarget ambulance photos to id numbers

--- app/helpers/ambulances_helper.rb
+++ app/helpers/ambulances_helper.rb
@@ -2,7 +2,7 @@ module AmbulancesHelper
   def ambulance_picture
     if @ambulance.show_img == true
       image_tag(
-        "https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/ambulances/#{@ambulance.shop_num}.png"
+        "https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/ambulances/#{@ambulance.id}.png"
       )
     else
       image_tag('https://s3.us-east-2.amazonaws.com/cfdshopnumbers/images/No_image_available.svg')
