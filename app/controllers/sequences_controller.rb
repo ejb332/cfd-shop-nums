0a58c44b16671183eb3d11fb5318cb5bc50805e9
Eric Biel <eric.j.biel@gmail.com>
2021-03-05 23:01:57 -0600

    Prettify dates

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -1,7 +1,6 @@
 class SequencesController < ApplicationController
   def index
     @sequences = Sequence.all.order(id: :asc)
-    # @sequences_trucks = Sequence.order(id: begins_with)
     render 'index.html.erb'
   end
 
